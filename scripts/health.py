aa3520b2f631e95eda6dc5fd083c4922d7571e6b
cristianzubcu <zubcu30@gmail.com>
2026-03-23 14:36:14 +0100

    Read service URLs from env vars, fall back to host.docker.internal

--- scripts/health.py
+++ scripts/health.py
@@ -1,14 +1,15 @@
+import os
 import requests
 
 SERVICES = {
-    "jellyfin":    "http://host.docker.internal:8096",
-    "radarr":      "http://host.docker.internal:7878",
-    "sonarr":      "http://host.docker.internal:8989",
-    "prowlarr":    "http://host.docker.internal:9696",
-    "qbittorrent": "http://host.docker.internal:8080",
-    "bazarr":      "http://host.docker.internal:6767",
-    "grafana":     "http://host.docker.internal:3001",
-    "prometheus":  "http://host.docker.internal:9090",
+    "jellyfin":    os.environ.get("JELLYFIN_URL",   "http://host.docker.internal:8096"),
+    "radarr":      os.environ.get("RADARR_URL",     "http://host.docker.internal:7878"),
+    "sonarr":      os.environ.get("SONARR_URL",     "http://host.docker.internal:8989"),
+    "qbittorrent": os.environ.get("QBIT_URL",       "http://host.docker.internal:8080"),
+    "prowlarr":    os.environ.get("PROWLARR_URL",   "http://host.docker.internal:9696"),
+    "bazarr":      os.environ.get("BAZARR_URL",     "http://host.docker.internal:6767"),
+    "grafana":     os.environ.get("GRAFANA_URL",    "http://host.docker.internal:3001"),
+    "prometheus":  os.environ.get("PROMETHEUS_URL", "http://host.docker.internal:9090"),
 }
 
 
